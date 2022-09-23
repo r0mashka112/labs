c477eb42c01150f1f2d6da157351922ca43c03af
r0mashka112 <114226705+r0mashka112@users.noreply.github.com>
2022-09-23 14:23:14 +0300

    Update lab.cpp

--- lab.cpp
+++ lab.cpp
@@ -9,14 +9,17 @@ int main()
 	cout << "Nechaev Roman Igorevich" << endl << "Group number: 221-332" << endl;
 
 	//exercise 2
-	cout << "min int: " << INT_MIN << endl << "max int: " << INT_MAX << endl;
-	cout << "min long: " << LONG_MIN << endl << "max long: " << LONG_MAX << endl;
-	cout << "min char: " << numeric_limits<char>::min() << endl << "max char: " << numeric_limits<char>::max() << endl;
-	cout << "min short: " << numeric_limits<short>::min() << endl << "max short: " << numeric_limits<short>::max() << endl;
-	cout << "min double: " << numeric_limits<double>::min() << endl << "max double: " << numeric_limits<double>::max() << endl;
-	cout << "min bool: " << numeric_limits<bool>::min() << endl << "max bool: " << numeric_limits<bool>::max() << endl;
-	cout << "min unsigned int: " << numeric_limits<unsigned int>::min() << endl << "max unsigned int: " << numeric_limits<unsigned int>::max() << endl;
-	cout << "min unsigned short: " << numeric_limits<unsigned short>::min() << endl << "max unsigned short: " << numeric_limits<unsigned short>::max() << endl;
+	int min_int = 0b10000000000000000000000000000000;
+	int max_int = 0b01111111111111111111111111111111;
+
+	cout << "sizeof int: " << sizeof(int) << endl << "min int: " << min_int << endl << "max int: " << max_int << endl;
+	cout << "sizeof long int: " << sizeof(long int) << endl << "min long: " << LONG_MIN << endl << "max long: " << LONG_MAX << endl;
+	cout << "sizeof char: " << sizeof(char) << endl << "min char: " << numeric_limits<char>::min() << endl << "max char: " << numeric_limits<char>::max() << endl;
+	cout << "sizeof short: " << sizeof(short) << endl << "min short: " << numeric_limits<short>::min() << endl << "max short: " << numeric_limits<short>::max() << endl;
+	cout << "sizeof double: " << sizeof(double) << endl << "min double: " << numeric_limits<double>::min() << endl << "max double: " << numeric_limits<double>::max() << endl;
+	cout << "sizeof bool: " << sizeof(bool) << endl << "min bool: " << numeric_limits<bool>::min() << endl << "max bool: " << numeric_limits<bool>::max() << endl;
+	cout << "sizeof unsigned int: " << sizeof(unsigned int) << endl << "min unsigned int: " << numeric_limits<unsigned int>::min() << endl << "max unsigned int: " << numeric_limits<unsigned int>::max() << endl;
+	cout << "sizeof unsigned short: " << sizeof(unsigned short) << endl << "min unsigned short: " << numeric_limits<unsigned short>::min() << endl << "max unsigned short: " << numeric_limits<unsigned short>::max() << endl;
 
 	//exercise 3
 	cout << "Enter number: " << endl;
